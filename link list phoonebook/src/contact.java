51ad90835addfc9d90974c0c3133414da1e6a514
mohammad <mohammad@DESKTOP-S8VFLMI>
2023-10-14 23:44:38 +0300

    some changes

--- link list phoonebook/src/contact.java
+++ link list phoonebook/src/contact.java
@@ -64,29 +64,6 @@ public class contact implements comparable<contact> {
 	public int compareto(contact c){
 		return contactname.compareTo(c.contactname);
 	}
-	public void read_contact(){ 
-		Scanner input=new Scanner(System.in);
-		System.out.println("Enter the contact's name:");
-		contactname=input.nextLine();
-		System.out.println("Enter the contact's phone number: ");
-		phonenumber=input.nextLine(); 
-		System.out.println("Enter the contact's email address: ");
-		email=input.nextLine();
-		System.out.println("Enter the contact's address: ");
-		address=input.nextLine();
-		System.out.println("Enter the contact's birthday: ");
-		birthday=input.nextLine();
-		System.out.println("Enter any notes for the contact: ");
-		notes=input.nextLine(); 
-	}
-	public void display() {
-		System.out.println("contactname:"+contactname);
-	}
-	public static void main(String[]args) {
-		contact c1 =new contact("aa","gg","1@1","hh","2020","jjjjjjjj");
-		contact c2 =new contact("bb","gg","1@1","hh","2020","jjjjjjjj");
-		c1.display();
-		System.out.println(c1.compareto(c2));
-	}
+	
 	
 }
